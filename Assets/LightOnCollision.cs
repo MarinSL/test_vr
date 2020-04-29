b20633ce26399eb4838510562e43cf0306b92e07
pterolen <kostrovmarina@gmail.com>
2020-04-29 16:36:09 +0200

    Scripts update

--- Assets/LightOnCollision.cs
+++ Assets/LightOnCollision.cs
@@ -2,24 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
+
 public class LightOnCollision : MonoBehaviour
 {
+    AudioSource audioSource;
+    bool triggered = false;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("trigger " + other.gameObject.tag);
-        if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("MainCamera"))
-            Destroy(gameObject);
+        if ((other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("MainCamera")) && !triggered)
+        {
+            StartCoroutine(playSound());
+            triggered = true;
+        }
+    }
+
+    IEnumerator playSound()
+    {
+        audioSource.PlayOneShot(audioSource.clip, 0.3f);
+        yield return new WaitForSeconds(audioSource.clip.length);
+        Destroy(gameObject);
+    }
+
+    public bool isTriggered()
+    {
+        return triggered;
     }
 }
--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,29 +4,27 @@ using UnityEngine;
 using Valve.VR;
 using Valve.VR.InteractionSystem;
 
+[RequireComponent(typeof(CharacterController))]
 public class Movement : MonoBehaviour
 {
     public SteamVR_Action_Vector2 input;
     public SteamVR_Action_Single speedMultiplyer;
     public float speed = 1;
     CharacterController cc;
-    Rigidbody rb;
+
     //public float speed;
 
     // Start is called before the first frame update
     void Start()
     {
         cc = GetComponent<CharacterController>();
-        rb = GetComponent<Rigidbody>();
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
-        cc.Move(speed * speedMultiplyer.axis * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up));
-        
-        rb.MovePosition(transform.position + (speed * speedMultiplyer.axis * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up)));
+
+        cc.Move(speed * speedMultiplyer.axis * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up) - new Vector3(0,9.81f,0)*Time.deltaTime);
     }
 }
